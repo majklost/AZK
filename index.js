a44e6d6a80e0aa6304698ffde9f598076b7471e1
Michal <majkl.mrkos@seznam.cz>
2022-05-22 15:16:58 +0200

    Finished asset loading, prepairing the hitArea

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-const Phaser = require("phaser")
+"use strict";
+//Determine resolution of window
+const xRes = 800;
+const yRes = 600;
+
+const Phaser = require("phaser");
 const config = {
-    type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create
-    }
+  type: Phaser.AUTO,
+  width: xRes,
+  height: yRes,
+  physics: {
+    default: "arcade",
+    arcade: {},
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
 };
 
 const game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+function preload() {
+  this.load.image("base_tile", require("./assets/basic_tile.png"));
 }
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-
-    const particles = this.add.particles('red');
-
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    const logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+function create() {
+  game.scale.scaleMode = Phaser.Scale.FIT;
+  // const shape = new Phaser.Geom.POLYGON();
+  const tile = this.add.image(xRes / 2, yRes / 2, "base_tile");
+  tile.setInteractive();
+  tile.on("pointerdown", function (pointer) {
+    console.log("shit");
+  });
 }
